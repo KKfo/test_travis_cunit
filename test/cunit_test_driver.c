ffcafef3fc1cacb9ddd0d313166a1f0efd2b6465
k <koolkat@localhost.localdomain>
2015-07-23 15:18:00 +0200

    cunit

--- test/cunit_test_driver.c
+++ test/cunit_test_driver.c
@@ -49,19 +49,19 @@ int main()
     // Run all tests using CUnit Basic interface which outputs
     // results to command-line.
     CU_basic_set_mode(CU_BRM_VERBOSE);
-    r = CU_basic_run_tests();
+    CU_basic_run_tests();
 
     // Run all tests using CUnit Automated interface which outputs
     // results to a file, default name CUnitAutomated-Results.xml.
     // DTD CUnit-Run.dtd and and XSL stylesheet CUnit-Run.xsl in Share/
     // Uncomment this line to override default output file prefix.
     // CU_set_output_filename("Test");
-    CU_list_tests_to_file();
+    //CU_list_tests_to_file();
 
     // Output listing of tests in suites to a file, default name
     // CUnitAutomated-Listing.xml
     // DTD CUnit-List.dtd and and XSL stylesheet CUnit-List.xsl in Share/
-    CU_automated_run_tests();
+    //CU_automated_run_tests();
 
     CU_cleanup_registry();
     return CU_get_number_of_tests_failed();
